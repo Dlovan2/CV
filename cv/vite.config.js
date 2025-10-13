6b01ebeb36ab2e44e5bac127b6c919590c81b663
Dlovan2 <117533200+Dlovan2@users.noreply.github.com>
2025-10-13 03:12:20 +0300

    Delete cv directory

--- cv/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  plugins: [vue()],
-  base: 'cv'  // replace with your GitHub repo name
-})
