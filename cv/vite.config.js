49c7dfcc738a046cfe72894b9c72163ec7a29649
Dlovan2 <117533200+Dlovan2@users.noreply.github.com>
2025-10-13 03:08:51 +0300

    Update vite.config.js

--- cv/vite.config.js
+++ cv/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: 'cv'  // replace with your GitHub repo name
 })
